1e112f6b64dbb64f3fbabb2ab61f2a0e2d46e178
allanx1ing <allanxing2003@gmail.com>
2022-10-31 10:55:36 -0700

    setup

---
+++ src/Components/SortingVisualizer.js
@@ -0,0 +1,48 @@
+import "../css/SortingVisualizer.css";
+import { useState, useEffect } from "react";
+
+const SortingVisualizer = () => {
+  useEffect(() => {
+    initiateArray();
+  }, []);
+
+  const [array, setArray] = useState([]);
+
+  const initiateArray = () => {
+    let temp = [];
+    for (let i = 0; i < 50; i++) {
+      temp.push(generateRandomNumber(1, 100));
+    }
+    setArray(temp);
+    console.log(array);
+  };
+
+  //   const [count, setCount] = useState(0);
+
+  return (
+    <div className="functionbar">
+      hello world
+      <button onClick={initiateArray}>button</button>
+      {array[0]}
+     
+      <div className="histogram">
+        {array.map((i) => (
+            <div className="hist-bar" key="key1">
+              {i}
+            </div>
+        ))}
+      </div>
+    </div>
+
+    // <div>
+    //   <p>You clicked {count} times</p>
+    //   <button onClick={() => setCount(count + 1)}>Click me</button>
+    // </div>
+  );
+};
+
+const generateRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+export default SortingVisualizer;
